233df01a2dc71731afdcd1d4f585a1ca48961352
Mohith GR <grmohith06@gmail.com>
2026-04-21 18:01:09 +0530

    Update solution for matrix-inverse

--- matrix-inverse/matrix-inverse.py
+++ matrix-inverse/matrix-inverse.py
@@ -3,11 +3,11 @@ import numpy as np
 def matrix_inverse(A):
     """
     Returns: A_inv of shape (n, n) such that A @ A_inv ≈ I
-    If matrix is singular or invalid, returns None.
     """
+    # Write code here
     A = np.array(A)
-
-    try:
-        return np.linalg.inv(A)
-    except np.linalg.LinAlgError:
-        return None
+    if np.linalg.det(A) != 0:
+        inverse = np.linalg.inv(A)
+    else:
+        inverse = None
+    return inverse
